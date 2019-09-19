62286007f3356be8190ba6da4f1eba1aa5b919a2
finn <10386624+printfinn@users.noreply.github.com>
2019-09-19 13:48:23 +0800

    Update sense-light.py
    Now the sensor won't be triggered by a bat anymore! bad bat! Spent me a lot of time!

--- sense-light.py
+++ sense-light.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import time
 from bluepy import btle
 import binascii
+import threading
+
 
 def now(): # type: String like 2000-01-01 14:38:40
     return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
@@ -23,8 +25,12 @@ def gen_interval(length, interval_type="MILLISECONDS"):
     else:  # In milliseconds
         return length, (length, interval_type)
 
+def log(text):
+    with open(log_filename, 'a') as log_file:
+        print(now() + " " + text, file=log_file)
 
-def open_garage_light():    
+def open_garage_light(): 
+
     '''
     ******* YOU NEED TO SETUP YOUR MAC_ADDRESS   *******
     ******* WHICH IS "FF:FF:FF:FF:FF:FF" NOW!    *******
@@ -33,20 +39,15 @@ def open_garage_light():
     Try to turn it on within `connect_ble_time_limit` seconds,
     if not successful (due to ble issues) then abort this try
     '''
-    mac_address = "ff:ff:ff:ff:ff:ff" # YOUR BLE switch mac address
-    connect_ble_time_limit = 60 # YOUR connect limit time in seconds
-
+    connect_ble_time_limit = 60 # in seconds
     time_limit = time.time() + connect_ble_time_limit
     dev = None # BLE device
     while (time.time() < time_limit):
-
-        # while BLE device is occupied, wait
-        while (dev is None and time.time() < time_limit): 
+        while (dev is None and time.time() < time_limit): # while BLE device is occupied, wait
             try:
-                dev = btle.Peripheral(mac_address)
+                dev = btle.Peripheral("ff:ff:ff:ff:ff:ff")
             except btle.BTLEException:
-                time.sleep(3)
-
+                time.sleep(0.5)
         # since now BLE switch is connected to RPi
         try:
             light_switch = btle.UUID("FFF0")
@@ -57,59 +58,151 @@ def open_garage_light():
             status = binascii.b2a_hex(status)
 
             if status[1] == 49: # ascii 1
-                # the light is already on
+                # print('the light is already on')
                 pass
-            elif status[1] == 48: # ascii 0
-                # the light is off, turning it on
+            if status[1] == 48: # ascii 0
+                # print('the light is off, turning it on')
                 switch_control.write(bytes("\x01", encoding='utf8'))
+                time.sleep(1) # give BLE some time to communicate
+            dev.disconnect() # Don't forget to disonnect BLE
+            break
 
-            # Don't forget to disonnect BLE
-            dev.disconnect() 
+        except btle.BTLEException:
+            dev = None
+            time.sleep(0.5)
+
+    success = True
+    if time.time() > time_limit:
+        return not success
+    else:
+        return success
+
+def close_garage_light():    
+    '''
+    ******* YOU NEED TO SETUP YOUR MAC_ADDRESS   *******
+    ******* WHICH IS "FF:FF:FF:FF:FF:FF" NOW!    *******
+    ******* CHANGE IT TO YOUR DEVICE MAC ADDRESS *******
+    Connect to your LOLAR Bluetooth Low Energy switch and turn it off;
+    Try to turn it off within `connect_ble_time_limit` seconds,
+    if not successful (due to ble issues) then abort this try
+    '''
+    connect_ble_time_limit = 60 # in seconds
+    time_limit = time.time() + connect_ble_time_limit
+    dev = None # BLE device
+    while (time.time() < time_limit):
+        while (dev is None and time.time() < time_limit): # while BLE device is occupied, wait
+            try:
+                dev = btle.Peripheral("ff:ff:ff:ff:ff:ff")
+            except btle.BTLEException:
+                time.sleep(3)
+        # since now BLE switch is connected to RPi
+        try:
+            light_switch = btle.UUID("FFF0")
+            light_service = dev.getServiceByUUID(light_switch)
+            switch_control = light_service.getCharacteristics()[0]
+            switch_status = light_service.getCharacteristics()[1]
+            status = switch_status.read()
+            status = binascii.b2a_hex(status)
+            if status[1] == 49: # ascii 1
+                # print('the light is on, turn it off')
+                switch_control.write(bytes("\x01", encoding='utf8'))
+                time.sleep(1) # give BLE some time to communicate
+            elif status[1] == 48: # ascii 0
+                # print('the light is already off')
+                pass
+
+            dev.disconnect() # Don't forget to disonnect BLE
             break
 
         except btle.BTLEException:
             dev = None
             time.sleep(3)
-
-
+    success = True
     if time.time() > time_limit:
-        return " [FAILED] Exceeds time limit, abort!"
+        return not success
     else:
-        return " [SUCCESS] Human detected, light is on!"
+        return success
+
+def double_check_is_human():
+    """
+    If the signal from sensor is only a short single square wave,
+    then after one second, there should not be high voltage any more.
+    we double check by detecting high voltage after a short pause,
+    because if its human moving around, there should be fairly high
+    possibility that RPi could sample another high voltage within 5 seconds
+    """
+    time.sleep(1)
+    log("[INFO] Double checking...")
+    t0 = time.time()
+    t1 = t0 + 5 # in seconds
+    is_human = False
+    while time.time() < t1:
+        time.sleep(0.2) # sample frequency, could decrease it to be more efficient
+        if GPIO.input(pin_control) == 1:
+            is_human = True
+            return is_human
+    if not is_human:
+        return False
 
 def human_sensed_callback(channel):
-    response = open_garage_light()
-    with open(log_filename, 'a') as log_file:
-        print(now() + " [Alert] Sensed something!", file=log_file)
-        print(now() + response, file=log_file)
-        print(now() + " [INFO] Will be sleeping for %d %s...\n" %(interval, interval_type), file=log_file)
+    GPIO.remove_event_detect(pin_control) # don't trigger event until done with the current one
+    log("[INFO] Sensed something, stopped listening to sensor.")
+
+    is_human = double_check_is_human() # in case of false positive trigger by sensor
+    if is_human:
+        log("[SUCCESS] Human confirmed!")
+        success = open_garage_light()
+        if success:
+            log("[SUCCESS] Light is on!")
+            log("[INFO] Closing light in %d seconds." % (time_before_close))
+            time.sleep(time_before_close)
+            closed_success = close_garage_light()
+            if closed_success:
+                log("[SUCCESS] Light is off!")
+            else:
+                log("[FAILED] BLE exceeds time limit, abort closing!")
+        else:
+            log("[FAILED] BLE exceeds time limit, abort opening!")
+    else:
+        log("[INFO] Not human. Sensor False Positive.")
+
+    GPIO.add_event_detect(pin_control, GPIO.RISING, bouncetime=detect_interval, callback=human_sensed_callback) # Setup event on rising edge
+    log("[INFO] Resumed listening to sensor.\n")
+
+
+
+
+
+
+
 
 
 pin_control = 14 #RPi BCM mode pin
 log_filename = 'sense_human_logs.txt'
-detect_interval, (interval, interval_type) = gen_interval(10, "MINUTES") # wait 10 minutes for another detection 
+detect_interval, (interval, interval_type) = gen_interval(300, "MILLISECONDS") # bounce time
+time_before_close = 60 * 5 # seconds
 
-# Init
+
 with open(log_filename, 'a') as log_file:
-    print(now() + " [INFO] Start sensing human")
     print("\n\n\n" + now() + " [INFO] Pi Human Sensor Starting...", file=log_file)
     print("[INFO] Configuration:", file=log_file)
     print("[INFO] \tPIN: Raspberry Pi BCM MODE %d" % (pin_control), file=log_file)
     print("[INFO] \tDETECT_INTERVAL: %d %s" % (interval, interval_type), file=log_file)
-    print(now() + " [INFO] Start sensing human...\n", file=log_file)
 
-# Use pi BCM mode
-GPIO.setmode(GPIO.BCM) 
 
-# Set pin 14 to be an input pin and set initial value to be pulled low
-GPIO.setup(pin_control, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+GPIO.setmode(GPIO.BCM) # Use BCM 2019.8.24
 
-# Setup event on rising edge
-GPIO.add_event_detect(pin_control, GPIO.BOTH, bouncetime=detect_interval, callback=human_sensed_callback) 
+GPIO.setup(pin_control, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 14 to be an input pin and set initial value to be pulled low (off)
+
+GPIO.add_event_detect(pin_control, GPIO.RISING, bouncetime=detect_interval, callback=human_sensed_callback) # Setup event on rising edge
+
 
 
 while(1):
     time.sleep(10)
 
 # GPIO.cleanup() # Clean up
+
+
+
 
